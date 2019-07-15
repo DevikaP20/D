94d97edf247224b99e6e349541f92d2a2b046242
DevikaP20 <52907351+DevikaP20@users.noreply.github.com>
2019-07-15 12:23:26 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,21 @@
+import ujava.util.*;
+public class hello
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n<0)
+{
+System.out.println("Negative");
+}
+else if(n>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
